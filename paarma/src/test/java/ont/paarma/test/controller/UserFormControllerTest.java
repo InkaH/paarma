9e47eb6d4acee5a044c8a0c30cbbd44f88a0559a
InkaH <inka.haltiapuu@gmail.com>
2016-09-29 00:58:58 +0300

    user form controller test + core implementation

--- paarma/src/test/java/ont/paarma/test/controller/UserFormControllerTest.java
+++ paarma/src/test/java/ont/paarma/test/controller/UserFormControllerTest.java
@@ -3,24 +3,28 @@ package ont.paarma.test.controller;
 //https://richardbarabe.wordpress.com/2008/08/14/test-driving-a-simpleformcontroller-with-spring-mvc/
 import static org.junit.Assert.assertEquals;
 import ont.paarma.config.AppConfig;
+import ont.paarma.controller.UserFormController;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.ui.ModelMap;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.servlet.ModelAndView;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AppConfig.class})
-public class UserControllerTest{
+@WebAppConfiguration
+public class UserFormControllerTest{
+	
 	@Autowired
 	UserFormController controller;
 	
 	@Test
 	public void testCreateUser(){
-		
-		ModelMap modelMap = new ModelMap();
-		String model = controller.initializeForm(modelMap);
-		assertEquals(model, "newUser");
+
+	    ModelAndView mv = controller.initializeForm();
+	    assertEquals(mv.getViewName(), "newUser");
 	}
 }
