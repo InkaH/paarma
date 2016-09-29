b8d6bd81890e8fb46c1dfe21fb74a43b0784cba0
InkaH <inka.haltiapuu@gmail.com>
2016-09-30 00:45:31 +0300

    added test & implementation to user form init

--- paarma/src/main/java/ont/paarma/controller/UserFormController.java
+++ paarma/src/main/java/ont/paarma/controller/UserFormController.java
@@ -1,15 +1,18 @@
 package ont.paarma.controller;
 
+import ont.paarma.model.User;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserFormController{
-	@RequestMapping(value = "/newUser.jsp", method = RequestMethod.GET)
-	public ModelAndView initializeForm() { 
-		ModelAndView model = new ModelAndView("newUser");
-		return model;
+	@RequestMapping(value = "/newUser", method = RequestMethod.GET)
+	public String setUpForm(Model model) { 
+		User user = new User();
+		model.addAttribute("user", user);
+		return "newUser";
 	}
 }
--- paarma/src/test/java/ont/paarma/test/controller/UserFormControllerTest.java
+++ paarma/src/test/java/ont/paarma/test/controller/UserFormControllerTest.java
@@ -1,30 +1,64 @@
 package ont.paarma.test.controller;
 
-//https://richardbarabe.wordpress.com/2008/08/14/test-driving-a-simpleformcontroller-with-spring-mvc/
-import static org.junit.Assert.assertEquals;
 import ont.paarma.config.AppConfig;
 import ont.paarma.controller.UserFormController;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AppConfig.class})
 @WebAppConfiguration
 public class UserFormControllerTest{
-	
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
 	@Autowired
 	UserFormController controller;
-	
-	@Test
-	public void testCreateUser(){
 
-	    ModelAndView mv = controller.initializeForm();
-	    assertEquals(mv.getViewName(), "newUser");
+	@Before
+	public void setUp() {
+
+		this.mockMvc = MockMvcBuilders
+				.webAppContextSetup(this.webApplicationContext).build();
+	}
+
+	@Test
+	public void testCreateUser() throws Exception{
+
+		this.mockMvc.perform(get("/newUser"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("newUser"))
+		.andExpect(forwardedUrl("/WEB-INF/views/newUser.jsp"))
+		.andExpect(model().attribute(
+			"user", hasProperty("id", is(0)))) //Hamcrest
+		.andExpect(model().attribute(
+			"user", hasProperty("firstName", nullValue()))) 
+		.andExpect(model().attribute(
+			"user", hasProperty("lastName", nullValue()))) 
+		.andExpect(model().attribute(
+			"user", hasProperty("streetAddress", nullValue()))) 
+		.andExpect(model().attribute(
+			"user", hasProperty("zipCode", nullValue()))) 
+		.andExpect(model().attribute(
+			"user", hasProperty("city", nullValue()))) 
+		.andExpect(model().attribute(
+			"user", hasProperty("phoneNumber", nullValue()))); 
 	}
 }
