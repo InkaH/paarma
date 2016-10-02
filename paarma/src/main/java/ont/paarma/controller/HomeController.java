6a61ad00df39cb6b662187bfc454845e06e3c559
InkaH <inka.haltiapuu@gmail.com>
2016-10-02 20:45:01 +0300

    testing git problems

--- paarma/src/main/java/ont/paarma/controller/HomeController.java
+++ paarma/src/main/java/ont/paarma/controller/HomeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 //@Controller annotation on class name declares this class as spring bean
 @Controller
-//@RequestMapping annotation declares that this class is default handler for all requests of type ‘/’
+//@RequestMapping annotation declares that this class is default handler for all requests of type ï¿½/ï¿½
 @RequestMapping("/")
 public class HomeController {
 	//First method does not have any mapping declared, it will inherit 
@@ -22,7 +22,7 @@ public class HomeController {
 	// ModelMap is a Map implementation, which here is acting as replacement of 
 	//[request.getAttribute()/request.setAttribute()], setting values as request attribute. 
 	public String sayHello(ModelMap model) {
-		model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+		model.addAttribute("greeting", "test");
 		//This value will be suffixed and prefixed with suffix and prefix defined in view resolver to form the real view file name.
 		return "index";
 	}
